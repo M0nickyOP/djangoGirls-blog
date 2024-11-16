662feb5f6e55030b919cf8281800852b799447cf
Monicky <mckyd.oliveira2007@gmail.com>
2024-11-16 14:10:05 -0300

    teste

---
+++ teste.py
@@ -0,0 +1,11 @@
+
+# def hi(nomes):
+#     print('Olá ' + nomes + '!')
+
+# pets =[ 'apollo','chane','brutus']
+# for nomes in pets:
+#     hi(nomes)
+
+for i in range (3,13):
+    if i % 2 == 0 :
+        print(i)
